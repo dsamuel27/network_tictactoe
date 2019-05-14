469206f8d058cfb111a5baf0a85075020e8a1a60
dsamuel27 <david.samuel1227@gmail.com>
2019-05-13 18:16:23 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame, sys
+
+pygame.init()
+
+size = width, height = 320, 240
+speed = [2, 2]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+
+    screen.fill(black)
+
+    pygame.display.flip()
